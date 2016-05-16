a6f1c61ccc442ed5d49de07a00184c223c4fa356
sinapam <sinapam@users.noreply.github.com>
2016-05-16 12:34:42 +0700

    changed mimetype to content_type

--- taggit_autosuggest_select2/views.py
+++ taggit_autosuggest_select2/views.py
@@ -24,10 +24,10 @@ def list_tags(request):
         values_list('name', flat=True)
     data = [{'name': n, 'value': n} for n in tag_name_qs[:limit]]
 
-    return HttpResponse(json.dumps(data), mimetype='application/json')
+    return HttpResponse(json.dumps(data), content_type='application/json')
 
 
 def list_all_tags(request):
     """Returns all the tags in the database"""
     all_tags = Tag.objects.all().values_list('name', flat=True)
-    return HttpResponse(json.dumps(list(all_tags)), mimetype='application/json')
+    return HttpResponse(json.dumps(list(all_tags)), content_type='application/json')
